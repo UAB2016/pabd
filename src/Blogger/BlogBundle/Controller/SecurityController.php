f60dbd3f6e09cbcef965d97067c5d8d3a09c34ce
Capriciu <mariusmaxa@yahoo.com>
2016-06-07 01:14:33 +0300

    Aceste sunt modificarile mele

---
+++ src/Blogger/BlogBundle/Controller/SecurityController.php
@@ -0,0 +1,65 @@
+<?php
+namespace Blogger\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class SecurityController extends Controller
+{
+	/**
+	 * @Route("/login", name="login_form")
+	 */
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render(
+            'BloggerBlogBundle:Security:login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $session->get(SecurityContextInterface::LAST_USERNAME),
+                'error'         => $error,
+            )
+        );
+    }
+	
+	    /**
+     * This is the route the login form submits to.
+     *
+     * But, this will never be executed. Symfony will intercept this first
+     * and handle the login automatically. See form_login in app/config/security.yml
+	/**
+	 * @Route("/login_check", name="login_check")
+	 */
+	public function loginCheckAction()
+	{
+	}
+		/**
+     * This is the route the user can use to logout.
+     *
+     * But, this will never be executed. Symfony will intercept this first
+     * and handle the logout automatically. See logout in app/config/security.yml
+     *
+     * @Route("/logout", name="security_logout")
+     */
+    public function logoutAction()
+    {
+        throw new \Exception('This should never be reached!');
+    }
+}
+
+
+
